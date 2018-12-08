3d2dc80ff5bf29b1ef4f6a24615e8afe69ae997b
Ciro Perfetto <ciroperfetto@hotmail.it>
2018-12-08 12:50:38 +0100

    Classi aggiornate

--- Classi/Film.java
+++ Classi/Film.java
@@ -2,32 +2,64 @@ package classi;
 
 import java.io.Serializable;
 
+/**
+ * Questa classe è immutabile(final) e viene utilizzata per salvare
+ * descizione, titolo e durata(in minuti) di un film.
+ */
 public final class Film implements Serializable{
+
+	private String descrizione;
+	private String titolo;
+	private int minuti;
+	private static final long serialVersionUID = 2958174258762431745L;
 	
+	/**
+	 * Il costruttore prende una Stringa titolo, una String descizione e un in per i minuti
+	 * @param titolo titolo del film
+	 * @param descrizione descrizione del film con breve trama
+	 * @param minuti durata del film in minuti
+	 */
 	public Film(String titolo, String descrizione, int minuti)
 	{
 		this.titolo = titolo;
 		this.descrizione = descrizione;
 	}
 	
+	/**
+	 * Metodo che ritorna il titolo del film
+	 * @return Stringa con titolo
+	 */
 	public String getTitolo()
 	{
 		return this.titolo;
 	}
 	
+	/**
+	 * Metodo che ritorna la descrizione del film
+	 * @return Stringa con descrizione
+	 */
 	public String getDescrizione()
 	{
 		return this.descrizione;
 	}
 	
+	/**
+	 * Metodo che ritorna la durata in minuti
+	 * @return int con durata in minuti
+	 */
 	public int getMinuti()
 	{
 		return this.minuti;
 	}
 	
+	/**
+	 * Metodo per stampare tutte le informazioni del film.
+	 * @return stringa con: titolo, descrizione e durata
+	 */
+	public String toString()
+	{
+		return getClass().getSimpleName() + "[Titolo = " + this.titolo + ", Descrizione = "
+				+ this.descrizione + ", Durata = " + this.minuti + "]";			
+	}
 	
-	private String descrizione;
-	private String titolo;
-	private int minuti;
-	private static final long serialVersionUID = 2958174258762431745L;
 }
--- Classi/Utente.java
+++ Classi/Utente.java
@@ -1,33 +1,112 @@
 package classi;
 
-public class Utente {
+import java.io.Serializable;
+
+/**
+ * Questa classe viene utilizzata per salvare
+ * username e password relativo ad un utente.
+ * Inoltre viene indicato se l'utente è un gestore e
+ * ha un ID progressivo.
+ */
+public class Utente implements Serializable{
 	
-	public Utente(String username, String password)
+	private static final long serialVersionUID = 8324800419465457181L;
+	private String username;
+	private String password;
+	/**
+	 * num_prog è una variabile statica che viene incrementata
+	 * ogni volta che viene creato un utente a cui viene assegnato come ID
+	 */
+	private static int num_prog = 0;
+	private int id;
+	private boolean isGestore;
+	
+	/**
+	 * Il costruttore prende username(stringa), password(stringa) e un boolean per
+	 * impostare o meno l'utente a gestore
+	 * @param username username dell'utente
+	 * @param password password dell'utente
+	 * @param isGestore indica se l'utente deve essere un gestore
+	 */
+	public Utente(String username, String password, Boolean isGestore)
 	{
 		this.username = username;
 		this.password = password;
+		this.isGestore = isGestore;
 		id = num_prog;
 		num_prog++;
 	}
 	
+	/**
+	 * Il metodo ritorna l'username dell'utente
+	 * @return Stringa con l'username
+	 */
 	public String getUsername()
 	{
 		return this.username;
 	}
 	
+	/**
+	 * Il metodo ritorna la password dell'utente
+	 * @return Stringa con la password
+	 */
 	public String getPassword()
 	{
 		return this.password;
 	}
 	
+	/**
+	 * Il metodo ritorna l'ID dell'utente
+	 * @return int con l'id
+	 */
 	public int getId()
 	{
 		return id;
 	}
 	
+	/**
+	 * Il metodo ritorna la variabile booleana isGestore
+	 * @return true se l'utente è un gestore, false altrimenti
+	 */
+	public boolean getGestore()
+	{
+		return this.isGestore;
+	}
 	
-	private String username;
-	private String password;
-	private static int num_prog = 0;
-	private int id;
+	/**
+	 * Metodo modificatore per cambiare l'username
+	 * @param username nuovo username dell'utente
+	 */
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+	
+	/**
+	 * Metodo modificatore per cambiare la password
+	 * @param password nuova password dell'utente
+	 */
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+	
+	/**
+	 * Metodo modificatore per cambiare i permessi dell'utente
+	 * @param bool true se l'utente deve essere gestore, false altrimenti
+	 */
+	public void setGestore(Boolean bool)
+	{
+		this.isGestore = bool;
+	}
+	
+	/**
+	 * Metodo per stampare tutte le informazione dell'Utente
+	 * @return stringa con: username, password, ID e permessi gestore
+	 */
+	public String toString()
+	{
+		return getClass().getSimpleName() + "[Username = " + this.username + ", Password = "
+				+ this.password + ", ID utente = " + this.id + ", E' un gestore = " + this.isGestore + "]";
+	}
 }
