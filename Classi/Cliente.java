a904f0974697b63398b11606ef4f385ea1a5dee3
Ciro Perfetto <ciroperfetto@hotmail.it>
2018-12-09 12:05:13 +0100

    Cambiamenti cliente e spettacolo

--- Classi/Cliente.java
+++ Classi/Cliente.java
@@ -1,6 +1,7 @@
 package classi;
 
 import java.util.ArrayList;
+
 /**
  * Rappresenta il concetto di Cliente come specializzazione di Utente. Un Cliente ha tutti i dati di un Utente ma 
  * possiede, in più, una collezione di prenotazioni effettuate (Collezione di Biglietti con stato = "non acquistato").
@@ -14,6 +15,7 @@ public class Cliente extends Utente{
 	 * Collezione di Biglietti con stato = "non prenotato" indicanti le prenotazioni attive del Cliente.
 	 */
 	private ArrayList<Biglietto> prenotazioni;
+	
 	/**
 	 * Metodo costruttore del Cliente che estende il costruttore dell'Utente aggiungendo ad esso la creazione
 	 * di una collezione di Biglietti (prenotazioni).
@@ -25,6 +27,7 @@ public class Cliente extends Utente{
 		super(username, password);
 		prenotazioni = new ArrayList<Biglietto>();
 	}
+	
 	/**
 	 * Metodo modificatore del Cliente che elimina la prenotazioni di indice i.
 	 * @param i L'indice della prenotazione da eliminare all'interno della collezione.
@@ -33,6 +36,7 @@ public class Cliente extends Utente{
 	{
 		prenotazioni.remove(i);
 	}
+	
 	/**
 	 * Metodo di accesso di Cliente che permette l'accesso ad una prenotazione di indice i.
 	 * @param i L'indice della prenotazione da prelevare nella collezione.
@@ -42,6 +46,7 @@ public class Cliente extends Utente{
 	{
 		return prenotazioni.get(i);
 	}
+	
 	/**
 	 * Metodo modificatore di aggiunta di una Prenotazione.
 	 * @param biglietto La prenotazione che si vuole aggiungere.
@@ -50,6 +55,7 @@ public class Cliente extends Utente{
 	{
 		prenotazioni.add(biglietto);
 	}
+	
 	/**
 	 * Metodo di controllo che indica se una Prenotazione è contenuta o meno nella collezione.
 	 * @param biglietto La Prenotazione da cercare nella collezione.
@@ -59,6 +65,7 @@ public class Cliente extends Utente{
 	{
 		return prenotazioni.indexOf(biglietto);
 	}
+	
 	/**
 	 * Metodo di visualizzazione che amplia il metodo toString() di Utente aggiungendo la stampa delle prenotazioni.
 	 * @return Restituisce una stringa contenente le informazioni principali del Cliente.
@@ -67,19 +74,21 @@ public class Cliente extends Utente{
 	{
 		return super.toString()+"[Prenotazioni = "+prenotazioni+"]";
 	}
+	
 	/**
 	 * Metodo di clonazione per un Cliente.
 	 * @return Restituisce un puntatore ad una copia del Cliente chiamante.
 	 */
 	public Cliente clone()
 	{
-			Cliente clone = (Cliente)super.clone();
-			ArrayList<Biglietto> biglietticlone = new ArrayList<Biglietto>();
-			for(Biglietto p : prenotazioni)
-				biglietticlone.add(p.clone());
-			clone.prenotazioni = biglietticlone;
-			return clone;
+		Cliente clone = (Cliente)super.clone();
+		ArrayList<Biglietto> biglietticlone = new ArrayList<Biglietto>();
+		for(Biglietto p : prenotazioni)
+			biglietticlone.add(p.clone());
+		clone.prenotazioni = biglietticlone;
+		return clone;
 	}
+	
 	/**
 	 * Metodo di controllo dell'uguaglianza tra un Cliente chiamante ed un Oggetto.
 	 * @param obj L'Oggetto con cui si vuole confrontare il Cliente chiamante.
