583f69feb05345d65a7921feb5c4981343e094a4
Ciro Perfetto <ciroperfetto@hotmail.it>
2018-12-11 22:04:59 +0100

    Create ModuloSpettacolo.java

---
+++ classi/ModuloSpettacolo.java
@@ -0,0 +1,125 @@
+package moduli;
+
+import java.util.Calendar;
+import classi.Film;
+import classi.Multisala;
+import classi.Spettacolo;
+import eccezioni.FilmNonPresenteException;
+import eccezioni.OraSpettacoloException;
+
+/**
+ * Classe per gestire gli Spettacoli di un multisala
+ */
+public class ModuloSpettacolo {
+	
+	private Multisala multisala;
+	
+	/**
+	 * Costruttore del moduloSpettacolo per gestire gli spettacoli del multisala
+	 * @param multisala Multisala da dove prendere la lista degli Spettacoli da gestire
+	 */
+	public ModuloSpettacolo(classi.Multisala multisala)
+	{
+		this.multisala = multisala;
+	}
+	
+	/**
+	 * Metodo che aggiunge uno spettacolo alla lista degli spettacoli del multisala.
+	 * Vengono passati nei parametri gli attributi per instanziare un nuovo spettacolo.
+	 * Poichè ogni spettacolo ha uno stato della sala diverso in base alle prenotazioni
+	 * viene assegnato al nuovo spettacolo una copia della sala in cui si vuole aggiungere lo
+	 * spettacolo
+	 * @param film Film dello spettacolo
+	 * @param numeroSala int con numero della sala
+	 * @param prezzo Double con prezzo dello spettacolo
+	 * @param data Calendar con data e orario dello spettacolo
+	 * @throws OraSpettacoloException eccezione lanciata nel caso in cui si tenta di aggiungere
+	 * lo spettacolo in un orario in cui è presente uno spettacolo ancora in corso nella sala7
+	 * in considerazione
+	 */
+	public void addSpettacolo(Film film, int numeroSala, Double prezzo, Calendar data) throws OraSpettacoloException
+	{
+		ModuloSala m = new ModuloSala(multisala);
+		Spettacolo spettacolo = new Spettacolo(m.getSala(numeroSala).clone(), film, data, prezzo);
+		
+		for (Spettacolo s: multisala.getListaSpettacoli())
+		{
+			if(s.getDataInizio().before(spettacolo.getDataInizio())
+					&& s.getDataFine().after(spettacolo.getDataInizio()) &&
+					s.getSala().equals(spettacolo.getSala()))
+				throw new OraSpettacoloException();
+		}
+		multisala.getListaSpettacoli().add(spettacolo);
+	}
+	
+	/**
+	 * Metodo per rimuovere uno spettacolo dalla lista spettacoli
+	 * @param id int con id dello spettacolo da rimuovere
+	 * @return true se lo spettacolo è stato rimosso, false altrimenti
+	 */
+	public boolean removeSpettacolo(int id)
+	{
+		Spettacolo s;
+		if ((s = getSpettacolo(id)) != null)
+			return multisala.getListaSpettacoli().remove(s);
+		else
+			return false;
+	}
+	
+	/**
+	 * Metodo che ritorna uno spettacolo cercato con l'id rispettivo
+	 * @param id int con id dello spettacolo
+	 * @return Spettacolo con id cercato se presente nella lista, null se non è presente
+	 */
+	public Spettacolo getSpettacolo(int id)
+	{
+		for (Spettacolo s: multisala.getListaSpettacoli())
+		{
+			if (s.getId() == id)
+				return s;
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Metodo per aggiungere un film alla lista dei film del multisala
+	 * @param f Film da aggiungere
+	 */
+	public void addFilm(Film f)
+	{
+		multisala.getListaFilm().add(f);
+	}
+	
+	/**
+	 * Metodo che rimuove un film dalla lista dei film di un multisala
+	 * @param idFilm int con id del film da eliminare
+	 * @return true se il film è stato eliminato, false altrimenti
+	 */
+	public boolean removeFilm(int idFilm)
+	{
+		Film f;
+		if ((f = getFilm(idFilm)) != null)
+				return multisala.getListaFilm().remove(f);
+		else
+			return false;
+	}
+	
+	/**
+	 * Metodo che ritorna un film cercato per id se presente nella lista dei film
+	 * @param filmId int con id del film
+	 * @return Film cercato per id se presente, altrimenti viene lanciata un'eccezione
+	 * @throws FilmNonPresenteException eccezione lanciata nel caso in cui un film cercato
+	 * non è presente nella lista dei film del multisala
+	 */
+	public Film getFilm(int filmId) throws FilmNonPresenteException
+	{
+		for (Film f: multisala.getListaFilm())
+		{
+			if (f.getIdFilm() == filmId)
+				return f;
+		}
+		
+		throw new FilmNonPresenteException();
+	}
+}
