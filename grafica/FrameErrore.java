63bdda6dfd776894c2734cb15713f4dd33389039
Hysse <Hysse@users.noreply.github.com>
2018-12-13 22:50:47 +0100

    Update FrameErrore.java

--- grafica/FrameErrore.java
+++ grafica/FrameErrore.java
@@ -3,12 +3,16 @@ package grafica;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import classi.Multisala;
+import listeners.CloseListener;
+
 public class FrameErrore extends JFrame{
 
-	public FrameErrore(Exception e)
+	public FrameErrore(Multisala multisala,Exception e)
 	{
-		setSize(100, 100);
+		setSize(500, 100);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addWindowListener(new CloseListener(multisala));
 		JLabel errore = new JLabel(e.getMessage());
 		add(errore);
 	}
